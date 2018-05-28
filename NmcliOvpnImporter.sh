1824c36b7298ba32a1dcb5416b15187d7fb25f68
Jeff Labonte <jeff.labonte12@gmail.com>
2018-05-27 20:53:56 -0400

    Update script to change VPN configurations

--- NmcliOvpnImporter.sh
+++ NmcliOvpnImporter.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
 
-cd ovpn_tcp/
-for f in *.ovpn; do
+import_ovpn(){
+     cd ovpn_tcp/
+     for f in *.ovpn; do
 	sudo nmcli connection import type openvpn file $f
+     done
+}
+
+set_credential_openvpn(){
+    local user
+    local password
+    for f in $(nmcli -f NAME connection show | grep nordvpn.com.tcp)
+    do
+	    echo $f
+            $sudo nmcli connection modify $f +vpn.data username=user +vpn.secrets password=password
+    done
+}
+
+while getopts ":u:p:" opt; do
+  case $opt in
+    u) au=$OPTARG   ;;
+    p) ap=$OPTARG   ;;
+    \?)       echo "Invalid option: -$OPTARG\n Please use parameter -h for help" >&2
+    exit 1 ;;
+
+  esac
 done
+
+
+
+if [ -z $au ]
+then
+  echo "-u [username] is required"
+  exit 1
+fi
+
+if [ -z $ap ]
+then
+  echo "-p [password] is required"
+  exit 1
+fi
+
+set_credential_openvpn $au $ap
